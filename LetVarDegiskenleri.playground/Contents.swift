d00e446d56e3b8c7eac803d39bf1cd7583e904fe
Sadettin Ayyildiz <ayyildiz.s83@gmail.com>
2020-08-05 11:09:35 +0300

    v1

--- LetVarDegiskenleri.playground/Contents.swift
+++ LetVarDegiskenleri.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
 var str = "Hello, playground"
+let str2 = "Hello world!"
+
+// var değişken tanımlar --> mutable (değiştirilebilir)
+// let constant (sabit) tanımlar --> immutable (değiştirilemez)
+
+// Değişken tanımlama yöntemleri
+var degisken1 : Int = 35
+let degisken2 : String = "Hello!"
+var degisken3 = "Hello!"
+degisken3 = "Hi!"
+let degisken4 = 68
+var degisken5 : String  // değişken tanımlanmış ancak başlatılmamış
+let degisken6 : Double  // değişken tanımlanmış ancak başlatılmamış
+var degisken7 : String = String()  // içine bos değer atanmış string
+var degisken8 : String = ""       // içine bos değer atanmış string
+
+let degisken9 : String
+degisken9 = "Örnek"
+
+
+
+
