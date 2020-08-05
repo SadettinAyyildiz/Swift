922ddfa9e62189c5770b9160dc16b91d07a28fd9
Sadettin Ayyildiz <ayyildiz.s83@gmail.com>
2020-08-05 20:47:58 +0300

    v1

--- 2-Donguler/5-WhileDongusu.playground/Contents.swift
+++ 2-Donguler/5-WhileDongusu.playground/Contents.swift
@@ -12,4 +12,19 @@ while i <= mesaj.count {
     i += 1
 }
 
+var j = 1
+while j <= 10 {
+    print(j*5)
+    j += 2
+}
 
+// Üretilen rasgele sayıların toplanması
+var toplam = 0
+var k = 0
+while k < 5 {
+    let rasgeleSayi = arc4random_uniform(6)  // o dan 5 e kadar rasgele sayı üretir
+    print("Üretilen sayı: \(rasgeleSayi)")
+    toplam += Int(rasgeleSayi)
+    k += 1
+}
+print("Rasgele üretilen 5 sayının toplamı \(toplam)")
